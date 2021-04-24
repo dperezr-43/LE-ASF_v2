a6575520fd44ae619c470921629dfe314adbb388
loganillan07 <jorge_illan@hotmail.com>
2021-04-23 19:41:46 -0500

    PRUEBA

--- Sistema/Modulos/Denuncias_IV/Seguimiento_Denuncia.aspx.designer.cs
+++ Sistema/Modulos/Denuncias_IV/Seguimiento_Denuncia.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace Sistema.Modulos.Denuncias_IV
     {
 
         /// <summary>
-        /// Control lblPrueba.
+        /// Control lblPrueba2.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblPrueba;
+        protected global::System.Web.UI.WebControls.Label lblPrueba2;
 
         /// <summary>
         /// Control lblDos.
