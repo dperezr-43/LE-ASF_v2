d9898f89e76138184a65b44e521dd693edd71be7
david-mabel <70763548+david-mabel@users.noreply.github.com>
2021-04-23 19:52:13 -0500

    Convinación de David

--- Sistema/Modulos/Denuncias_IV/Seguimiento_Denuncia.aspx.designer.cs
+++ Sistema/Modulos/Denuncias_IV/Seguimiento_Denuncia.aspx.designer.cs
@@ -15,12 +15,12 @@ namespace Sistema.Modulos.Denuncias_IV
     {
 
         /// <summary>
-        /// Control lblDos.
+        /// Control lblDos2.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lblDos;
+        protected global::System.Web.UI.WebControls.Label lblDos2;
     }
 }
